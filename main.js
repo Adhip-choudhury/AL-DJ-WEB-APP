4b94a5c53664c7e5e5a0674273ae7a80570fc6b6
Adhip-choudhury <79558085+Adhip-choudhury@users.noreply.github.com>
2021-10-10 19:18:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
  music="";
+ leftWristx=0;
+ rightWristx=0;
 
-
+ leftWristy=0;
+ rightWristy=0;
 
 function preload(){
    music=loadSound("music.mp3");
@@ -13,15 +16,43 @@ function setup(){
   video=createCapture(VIDEO);
   video.hide();
 
+  poseNet= ml5.poseNet(video , modelLoaded);
+  poseNet.on("pose", gotResult);
 }
 
 
 function draw(){
    image(video, 0, 0, 400, 300);
 
+
+   fill("#ff391e");
+   stroke("#ff391e");
+   circle(leftWristx, leftWristy, 20);
 }
 
 
 function PLAY(){
     music.play();
+    music.setVolume(1);
+    music.rate(1);
+}
+
+
+function modelLoaded(){
+   console.log("model is loaded");
+}
+
+
+function gotResult(results){
+    if(  results.length > 0){
+      console.log(results);
+      leftWristx=results[0].pose.leftWrist.x;
+      leftWristy=results[0].pose.leftWrist.y;
+      console.log("leftWristx: " +leftWristx+ " leftWristy: " +leftWristy);
+
+      
+      rightWristx=results[0].pose.rightWrist.x;
+      rightWristy=results[0].pose.rightWrist.y;
+      console.log("rightWristx: " +rightWristx+ " rightWristy: " +rightWristy);
+    }  
 }
